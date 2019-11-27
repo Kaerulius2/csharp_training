fea6fb1a6c9040b9f7e7ecb88789e779e1bfbe81
kaerulius2@yandex.ru <kaerulius2@yandex.ru>
2019-11-27 21:10:04 +0300

    Создан проект для тестирования MantisBT

---
+++ mantis-tests/mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        public RegistrationHelper Registration { get; set; }
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/";
+            Registration = new RegistrationHelper(this);
+          
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (! app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                //newInstance.Navigator.OpenHomePage();
+                newInstance.driver.Url = "localhost";
+                app.Value = newInstance;
+                
+            }
+            return app.Value;
+        }
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+
+        }
+
+        
+
+       
+
+        public IWebDriver Driver {
+            get
+            {
+                return driver;
+            }
+        }
+        
+    }
+}
---
+++ mantis-tests/mantis-tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        internal void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+
+        }
+
+        private void OpenRegistrationForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SubmitRegistration()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FillRegistrationForm(AccountData account)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantis/login_page.php";
+        }
+    }
+}
